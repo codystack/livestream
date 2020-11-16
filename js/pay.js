58f7eaccc6e385760561471b3c173e4c03d3f61e
codystack <ebuzzadvert@gmail.com>
2020-11-16 14:31:03 +0100

    V1

--- js/pay.js
+++ js/pay.js
@@ -4,18 +4,19 @@ function payWithPaystack(e) {
     e.preventDefault();
     var username=document.getElementById("username").value;
     var otherdata = {username:username, lastName:"nothing"};
+    var myref=Math.floor((Math.random() * 1000000000) + 1);
     let handler = PaystackPop.setup({
         key: 'pk_test_274c204e073e2b9d908430573ba1603a843c66d4', // Replace with your public key
         email: document.getElementById("email").value,
         metadata: otherdata,
         amount: document.getElementById("amount").value * 100,
-        ref: ''+Math.floor((Math.random() * 1000000000) + 1),
+        ref: ''+myref,
         onClose: function(){
 
         },
         callback: function(response){
-            if(myref===response.reference){
-                location.replace("https://live.nigerianqueen.org/regsuccess")
+            if(myref==response.reference){
+                location.replace("regsuccess")
             }
         }
     });
