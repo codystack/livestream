704e79b482ef4f8da14482f1794efccf90546bff
codystack <ebuzzadvert@gmail.com>
2020-11-22 14:55:12 +0100

    V1.1

--- dashboard.php
+++ dashboard.php
@@ -17,7 +17,7 @@ require_once "./config/controller.php";
 						<div class="col-md-12 col-lg-8 col-xl-8">
 							<div class="card">
                                 <div class="embed-responsive embed-responsive-16by9">
-                                    <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/nIyLXzXHdQ4" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                                    <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/-nG3SUBhLqE" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
                                 </div>
 							</div>
 						</div>
@@ -59,7 +59,7 @@ require_once "./config/controller.php";
                                             </div>
                                         </div>
                                     </div>
-                                    <h1 class="display-5 mt-1 mb-3">Preliminary Show</h1>
+                                    <h1 class="display-5 mt-1 mb-3">Fashion Show</h1>
                                     <div class="mb-0">
                                         22nd Nov 2020 | 1:00PM (WAT)
                                     </div>
