6d1474bbbea8738df09cbc8db5c0ce6ad92ce9ad
codystack <ebuzzadvert@gmail.com>
2020-11-22 10:20:07 +0100

    V1.1

--- dashboard.php
+++ dashboard.php
@@ -17,7 +17,7 @@ require_once "./config/controller.php";
 						<div class="col-md-12 col-lg-8 col-xl-8">
 							<div class="card">
                                 <div class="embed-responsive embed-responsive-16by9">
-                                    <iframe class="embed-responsive-item" src="https://www.youtube-nocookie.com/embed/4FniT0EX7g8" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                                    <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/4FniT0EX7g8" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
                                 </div>
 							</div>
 						</div>
@@ -61,7 +61,7 @@ require_once "./config/controller.php";
                                     </div>
                                     <h1 class="display-5 mt-1 mb-3">Preliminary Show</h1>
                                     <div class="mb-0">
-                                        22nd Nov 2020 | 10:00AM (WAT)
+                                        22nd Nov 2020 | 12:00PM (WAT)
                                     </div>
 
                                 </div>
