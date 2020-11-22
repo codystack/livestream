a4e86ac29e16f7e73354ed53c69739a68b8c6c65
codystack <ebuzzadvert@gmail.com>
2020-11-22 10:50:12 +0100

    V1.1

--- dashboard.php
+++ dashboard.php
@@ -17,7 +17,7 @@ require_once "./config/controller.php";
 						<div class="col-md-12 col-lg-8 col-xl-8">
 							<div class="card">
                                 <div class="embed-responsive embed-responsive-16by9">
-                                    <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/4FniT0EX7g8" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                                    <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/X89_Uc-3KBw" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
                                 </div>
 							</div>
 						</div>
