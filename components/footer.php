1d1aa54b784778e60ea2162105bcf6f36d68fe70
codystack <ebuzzadvert@gmail.com>
2020-11-22 06:08:47 +0100

    V1.1

--- components/footer.php
+++ components/footer.php
@@ -39,74 +39,6 @@
 
 <script>
     $(function() {
-        // Line chart
-        new Chart(document.getElementById("chartjs-dashboard-line"), {
-            type: 'line',
-            data: {
-                labels: ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"],
-                datasets: [{
-                    label: "Orders",
-                    fill: true,
-                    backgroundColor: window.theme.primary,
-                    borderColor: window.theme.primary,
-                    borderWidth: 2,
-                    data: [3, 2, 3, 5, 6, 5, 4, 6, 9, 10, 8, 9]
-                },
-                    {
-                        label: "Sales ($)",
-                        fill: true,
-                        backgroundColor: "rgba(0, 0, 0, 0.05)",
-                        borderColor: "rgba(0, 0, 0, 0.05)",
-                        borderWidth: 2,
-                        data: [5, 4, 10, 15, 16, 12, 10, 13, 20, 22, 18, 20]
-                    }
-                ]
-            },
-            options: {
-                maintainAspectRatio: false,
-                legend: {
-                    display: false
-                },
-                tooltips: {
-                    intersect: false
-                },
-                hover: {
-                    intersect: true
-                },
-                plugins: {
-                    filler: {
-                        propagate: false
-                    }
-                },
-                elements: {
-                    point: {
-                        radius: 0
-                    }
-                },
-                scales: {
-                    xAxes: [{
-                        reverse: true,
-                        gridLines: {
-                            color: "rgba(0,0,0,0.0)"
-                        }
-                    }],
-                    yAxes: [{
-                        ticks: {
-                            stepSize: 5
-                        },
-                        display: true,
-                        gridLines: {
-                            color: "rgba(0,0,0,0)",
-                            fontColor: "#fff"
-                        }
-                    }]
-                }
-            }
-        });
-    });
-</script>
-<script>
-    $(function() {
         // Bar chart
         new Chart(document.getElementById("chartjs-dashboard-bar-alt"), {
             type: "bar",
@@ -158,67 +90,6 @@
         });
     });
 </script>
-<script>
-    $(function() {
-        $("#usa_map").vectorMap({
-            map: "us_aea",
-            backgroundColor: "transparent",
-            zoomOnScroll: false,
-            normalizeFunction: "polynomial",
-            hoverOpacity: .7,
-            hoverColor: false,
-            regionStyle: {
-                initial: {
-                    fill: "#DCE3E8"
-                }
-            },
-            markerStyle: {
-                initial: {
-                    "r": 9,
-                    "fill": window.theme.primary,
-                    "fill-opacity": .9,
-                    "stroke": "#fff",
-                    "stroke-width": 7,
-                    "stroke-opacity": .4
-                },
-                hover: {
-                    "stroke": "#fff",
-                    "fill-opacity": 1,
-                    "stroke-width": 1.5
-                }
-            },
-            markers: [{
-                latLng: [37.77, -122.41],
-                name: "San Francisco: 375"
-            }, {
-                latLng: [40.71, -74.00],
-                name: "New York: 350"
-            }, {
-                latLng: [39.09, -94.57],
-                name: "Kansas City: 250"
-            }, {
-                latLng: [36.16, -115.13],
-                name: "Las Vegas: 275"
-            }, {
-                latLng: [32.77, -96.79],
-                name: "Dallas: 225"
-            }]
-        });
-        setTimeout(function() {
-            $(window).trigger('resize');
-        }, 350)
-    })
-</script>
-<script>
-    $(function() {
-        $('#datatables-dashboard-products').DataTable({
-            pageLength: 5,
-            lengthChange: false,
-            bFilter: false,
-            autoWidth: false
-        });
-    });
-</script>
 
 <script>
     var time = new Date().getHours();
@@ -232,9 +103,6 @@
     document.getElementById("greet").innerHTML = greeting;
 </script>
 
-
-
-
 <script type="text/javascript">
     function myFunction() {
         var copyText = document.getElementById("refLink");
@@ -244,18 +112,23 @@
     }
 </script>
 <!-- Make Deposit Modal End -->
-
-
 <script type="text/javascript">
     function myFunction() {
         var copyText = document.getElementById("refLink");
         copyText.select();
         copyText.setSelectionRange(0, 99999);
         document.execCommand("copy");
-        alert("Ref link copied: " + copyText.value);
+        alert("Access Code Copied: " + copyText.value);
     }
 </script>
-
+<script type="text/javascript">
+    $(document).ready(function()
+    { 
+        $(document).bind("contextmenu",function(e){
+                return false;
+        }); 
+    })
+</script>
 
 </body>
 
